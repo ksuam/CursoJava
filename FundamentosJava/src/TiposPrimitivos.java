2a132da6475c9bb6dbf719d701023be4a4a25135
ksuam <ksuamartinez@gmail.com>
2024-06-18 14:12:24 -0500

    first java

---
+++ FundamentosJava/src/TiposPrimitivos.java
@@ -0,0 +1,13 @@
+public class TiposPrimitivos {
+    public static void main(String args[]){
+
+        /*
+         tipos primitivos enteros: byte, short, int, long
+         */
+
+         byte numeroByte =10;
+         System.out.println(numeroByte);
+
+
+    }
+}
