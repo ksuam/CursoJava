e2adb83c2cc7094ba4d4d42b0697a13041fb4e65
ksuam <ksuamartinez@gmail.com>
2024-06-18 16:50:09 -0500

    Tipos primitivos

--- FundamentosJava/src/TiposPrimitivos.java
+++ FundamentosJava/src/TiposPrimitivos.java
@@ -8,6 +8,56 @@ public class TiposPrimitivos {
          byte numeroByte =10;
          System.out.println(numeroByte);
 
+         short numeroShort = 10;
+         System.out.println("Numero Short: "+ numeroShort);
+         System.out.println("Valor minimo Short: "+ Short.MIN_VALUE);
+         System.out.println("Valor maximo Short: "+ Short.MAX_VALUE);
+
+         short numeroInt = 10;
+         System.out.println("Numero Short: "+ numeroInt);
+         System.out.println("Valor minimo Short: "+ Integer.MIN_VALUE);
+         System.out.println("Valor maximo Short: "+ Integer.MAX_VALUE);
+
+         short numeroLong = 10;
+         System.out.println("Numero Short: "+ numeroLong);
+         System.out.println("Valor minimo Short: "+ Long.MIN_VALUE);
+         System.out.println("Valor maximo Short: "+ Long.MAX_VALUE);
+
+        /*
+         Tipos prmitivos de tipo flotante: float, double
+         */
+
+        float numeroFloat = (float)10.0;
+        System.out.println(numeroFloat);    
+        
+        double numeroDouble = 10;
+        System.out.println("Numero double: "+ numeroDouble);
+        System.out.println("Valor maximo double: "+ Double.MAX_VALUE);
+        System.out.println("Valor maximo double: "+ Double.MIN_VALUE);
+
+
+        /* Inferencia de tipos */
+
+        var numeroEntero = 10;
+        System.out.println("NumeroEntero = "+numeroEntero);
+
+        //Tipos primitivos tipo char
+
+        char miCaracter = 'a';
+        System.out.println("caracter = "+miCaracter);
+
+        //0021 = !
+        char varChar = '\u0021';  // se usa backslashu para indicar que se va a usar unicode
+        System.out.println("Variable unicode: "+varChar);
+        
+        // cahr en codigo decimal
+        char varCharDecimal = 33;
+        System.out.println("varchardecimal: "+varCharDecimal);
+
+
+
+
+
 
     }
 }
